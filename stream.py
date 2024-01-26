b75f0ac966ba21da852b2fc0fd5c2859c241652d
dexboostertech <157842830+dexboostertech@users.noreply.github.com>
2024-01-26 21:14:33 +0100

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,24 +1,23 @@
 import streamlit as st
 import requests
-import pandas as pd
-
-# Crear desplegable para que el usuario elija la red
-network = st.selectbox('Selecciona la red', ('Ethereum', 'Polygon', 'Arbitrum'))
-
-# Crear la URL final para la solicitud a la API
-url = 'https://open.dextools.io/free/v2/dex/arbitrum?sort=name&order=asc'
 
 # Realizar la solicitud a la API
-response = requests.get(url, headers={'accept': 'application/json', 'X-BLOBR-KEY': 'Kf8DMH7o176auOp0jD9HNnyqEzJc6P'})
+response = requests.get('https://open-api.dextools.io/free/v2/dex/arbitrum?sort=name&order=asc', 
+                        headers={'accept': 'application/json', 'X-BLOBR-KEY': 'Kf8DMH7o176auOp0jD9YYHNnyqEzJc6P'})
 
-# Verificar si la solicitud fue exitosa y procesar los datos
+# Verificar si la solicitud fue exitosa
 if response.status_code == 200:
-    data = response.json()['data']['results']
-    
+    data = response.json()
+    results = data['data']['results']
+
     # Crear un DataFrame con los resultados
-    df = pd.DataFrame(data)
-    
-    # Mostrar el gráfico de barras con el valor de volume24h
-    st.bar_chart(df['volume24h'])
+    df = pd.DataFrame(results)
+
+    # Mostrar los datos en una tabla utilizando Streamlit
+    st.write("Datos obtenidos de la API:")
+    st.table(df)
+
+    # Crear un gráfico de barras del volumen
+    st.bar_chart(df['volumen'])
 else:
     st.write(f"Error al obtener los datos de la API. Código de estado: {response.status_code}")
